d2f03bb24b2b1168b24dddfad5c6471c509f82ae
francobrida <francobrida@gmail.com>
2026-02-24 21:51:03 +0100

    Rename 'name' to 'nickname' in User model and migration

--- app/Enums/UserRoles.php
+++ app/Enums/UserRoles.php
@@ -9,16 +9,4 @@ enum UserRole :string
     case CLUBBER = 'clubber';
 }
 
-/*
-en el modelo:
-protected function casts(): array
-{
-    return [
-        'role' => \App\Enums\UserRole::class,
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
-}
-
-*/
 ?>
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'nickname',
         'email',
         'password',
         'role',
@@ -59,7 +59,7 @@ class User extends Authenticatable
      */
     public function initials() : string
     {
-        return Str::of($this->name)
+        return Str::of($this->nickname)
             ->explode(' ')
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
